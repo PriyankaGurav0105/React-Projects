78e2417d0820cfdbf074f328f08e84c4687d82df
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2024-08-29 22:10:03 +0530

    using objects and using {} brackets in jsx

--- demoproject/src/App.js
+++ demoproject/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+// let name = "Priyanka Gurav";
+let person ={
+  name: "Priyanka Gurav",
+  age: 25,
+  occupation: "Software Developer"
+}
 function App() {
   return (
     <>
@@ -10,6 +16,7 @@ function App() {
       <li>Contact</li>
       </nav>
       <div className="container">
+        <h2>Website developed by {person.name} age {person.age} and is working as {person.occupation}.</h2>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
           Excepturi ducimus optio, amet ex, recusandae sit at totam earum tempore ut voluptates dignissimos 
           voluptatibus accusamus dicta accusantium quis, deserunt nesciunt maxime!</p>
